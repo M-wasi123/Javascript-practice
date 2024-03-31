ab90dadb12fa387c540c6992f9b512dc6d37b44a
M-wasi123 <wasikhatri11@gmail.com>
2024-03-31 19:33:26 +0500

    new

---
+++ sw.js
@@ -0,0 +1,28 @@
+// Version
+var appVersion = 'v1.00';
+
+// File to cache
+var files = [
+    './',
+    './index.html',
+    'style.css'
+]
+
+// Install
+self.addEventListener('install', Event =>{
+    Event.waitUntil(
+        caches.open(appVersion)
+        .then(cache=> {
+            return cache.addAll(files)
+            .catch(err=> {
+                console.log('Error adding file to cache', err);
+            })
+        })
+    )
+    console.info('sw installed');
+    self.skipWaiting();
+})
+// Activate
+
+
+// Fetch
